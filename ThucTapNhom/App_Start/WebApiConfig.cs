2ed2d7643fe01057af4afbbe6a7542e9fe6d5bee
Believe <namph@meeyland.com>
2020-07-01 01:07:20 +0700

    enable cors

--- ThucTapNhom/App_Start/WebApiConfig.cs
+++ ThucTapNhom/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ThucTapNhom
 {
@@ -10,7 +11,8 @@ namespace ThucTapNhom
         public static void Register(HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
-
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
